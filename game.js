91746763d4997394169690a9e29e68755ed0276d
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-02-11 20:39:37 +0100

    Implement simple main playgame area

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 initGame();
 
 function initGame() {
-
-    // Your game can start here, but define separate functions, don't write everything in here :)
-
+  const main = document.querySelector(".main");
+  main.style.width = main.style.height;
+  // Your game can start here, but define separate functions, don't write everything in here :)
 }
