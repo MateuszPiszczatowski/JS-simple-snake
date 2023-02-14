743a923d175431c02c6f57634ad78a3c1e89a3da
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-02-14 20:30:34 +0100

    Implement snake movement

--- js/game-engine.js
+++ js/game-engine.js
@@ -1,12 +1,34 @@
 "use strict";
-import { initSnake } from "./snake.js";
+import { initSnake, moveSnake } from "./snake.js";
 import { prepareGameBoard } from "./gameboard.js";
 
+const KEY_CONF = {
+  w: "up",
+  W: "up",
+  s: "down",
+  S: "down",
+  a: "left",
+  A: "left",
+  d: "right",
+  D: "right",
+};
+
 const gameEngine = (grid, snake) => {
+  const keyPressedEvent = (event) => {
+    const pressedKey = event.key;
+    if (Object.hasOwn(KEY_CONF, pressedKey)) {
+      snake.head.dataset.direction = KEY_CONF[event.key];
+    }
+  };
+  const body = document.querySelector("body");
+  body.addEventListener("keydown", keyPressedEvent);
+  const interval = setInterval(moveSnake, 300, snake, grid);
   function endGame() {
     grid.innerHTML = "";
     grid.style.display = "none";
     document.querySelector(".main").classList.remove("main--game");
+    body.removeEventListener("keydown", keyPressedEvent);
+    clearInterval(interval);
   }
   snake.head.addEventListener("click", endGame);
 };
@@ -14,7 +36,9 @@ const gameEngine = (grid, snake) => {
 const initGame = () => {
   const grid = prepareGameBoard();
   const snake = initSnake(grid);
-  gameEngine(grid, snake);
+  setTimeout(() => {
+    gameEngine(grid, snake);
+  }, 3000);
 };
 
 export { initGame };
