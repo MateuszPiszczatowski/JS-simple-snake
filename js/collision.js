38f7ccfef49cf7cabb2744e4fe35ac0ac5661576
marcioszka <monkiewicz.marta@gmail.com>
2023-02-16 15:48:51 +0100

    Define funcs to check collision with food and walls

--- js/collision.js
+++ js/collision.js
@@ -1,7 +1,36 @@
 "use strict";
-import { initSnake, setGridStyle } from "./snake";
+import { getGridCoords } from "./snake.js";
+import { initGame } from "./game-engine.js";
 
-const checkIfCollision = () => {
 
-    return -1;
+function checkIfFoodCollision(snake, apple) {
+    const snakeHeadCoords = getGridCoords(snake.head);
+    const appleCoords = getGridCoords(apple);
+    return snakeHeadCoords === appleCoords;
+};
+
+function handleFoodCollision() {
+    /* elongate snake */
+};
+
+function checkIfWallCollision(grid){
+    const snakeHeadCoords = getGridCoords(snake.head);
+    const width = grid.offsetWidth;
+    const height = grid.offsetHeight;
+    return snakeHeadCoords[0] >= width || snakeHeadCoords[0] <= width
+        || snakeHeadCoords[1] >= height || snakeHeadCoords[1] <= height;
+};
+
+function handleWallCollision(){
+    if (confirm("Game over, you looooooser!")) {initGame()}
+    else {
+        /* displayScore() */
+    };
+};
+
+function checkSnakeCollision(snake){
+    const snakeHeadCoords = getGridCoords(snake.head);
+    const snakeTailCoords = getGridCoords(snake.tail);
+    const snakeBodyCoords = getGridCoords(snake.body);
+    
 }
