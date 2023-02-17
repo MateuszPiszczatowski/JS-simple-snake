e95fa89d6f425427ce2136be02612d1afb0c2c04
EmOsi22 <m.osiecka.222@gmail.com>
2023-02-17 01:24:28 +0100

    Add other friuts

--- js/friuts.js
+++ js/friuts.js
@@ -1,4 +1,9 @@
 "use strict";
+
+import { setGridStyle } from "./snake";
+
+
+
 const initApple = () => {
     const apple = document.createElement("div");
     apple.classList.add("apple");
@@ -11,3 +16,21 @@ const initApple = () => {
     }while(squares[appleIndex].classList.contains("snake")) 
     squares[appleIndex].classList.add("apple") 
     } 
+const initOrange = () => {
+    const orange = document.createElement("div");
+    orange.classList.add("orange");
+    setGridStyle(orange, 10, 10);
+    return orange;
+    };
+const initPear = () => {
+    const pear = document.createElement("div");
+    pear.classList.add("pear");
+    setGridStyle(pear, 10, 10);
+    return pear
+}
+const initCherries = () => {
+    const cherries = document.createElement("div")
+    cherries.classList.add("cherries")
+    setGridStyle(cherries, 10, 10);
+    return cherries
+}
